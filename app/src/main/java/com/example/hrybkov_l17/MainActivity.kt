e3e28bfa2b09f623d207d4d27278744e452bc6fa
Dmytro Hrybkov <hiukolp@gmail.com>
2021-01-28 13:53:37 +0200

    initial commit

---
+++ app/src/main/java/com/example/hrybkov_l17/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hrybkov_l17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
